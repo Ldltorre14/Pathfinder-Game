25ac85f4c7b139f2c99f2428c09b3072fff9167c
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-05-21 09:16:47 -0600

    Update board.py
    -Board_to_Matrix method added

--- board.py
+++ board.py
@@ -9,19 +9,29 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 
-import pygame
-
 class GameBoard:
     def __init__(self, screen):
         self.screenWidth, self.screenHeight = screen.get_size()
-        tile_size = 10
-        margin = 3  # Margin between tiles
-        self.grid_width = self.screenWidth // (tile_size + margin) 
-        self.grid_height = (self.screenHeight // (tile_size + margin)) - 20
-        
-        self.grid = [[Tile((x * (tile_size + margin)) + margin, ((y * (tile_size + margin)) + margin) + 235 , tile_size, tile_size, (0,255,255), False) for y in range(self.grid_height)] for x in range(self.grid_width)]
+        self.tile_size = 10
+        self.margin = 3  # Margin between tiles
+        self.grid_width = self.screenWidth // (self.tile_size + self.margin) 
+        self.grid_height = (self.screenHeight // (self.tile_size + self.margin)) - 20 
+        self.grid = [[Tile((x * (self.tile_size + self.margin)) + self.margin, ((y * (self.tile_size + self.margin)) + self.margin) + 235 , self.tile_size, self.tile_size, (255,255,255), False) for y in range(self.grid_height)] for x in range(self.grid_width)]
         
     def draw_board(self, screen):
         for row in self.grid:
             for tile in row:
                 tile.draw(screen)
+    
+    def board_to_matrix(self):
+        matrix = [[' ' for _ in range(self.grid_height)] for _ in range(self.grid_width)]
+        for i in range(self.grid_width):
+            for j in range(self.grid_height):
+                if self.grid[i][j].flags:
+                    matrix[i][j] = 'X'
+        return matrix
+    
+
+
+
+
