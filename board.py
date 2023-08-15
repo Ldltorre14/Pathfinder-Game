6b70f27fbd9b442a4ab75e43233665dbd065201e
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-08-14 22:17:22 -0600

    Add files via upload

--- board.py
+++ board.py
@@ -12,7 +12,7 @@ class GameBoard:
         self.margin = 3  # Margin between tiles
         self.grid_width = self.screenWidth // (self.tile_size + self.margin) 
         self.grid_height = (self.screenHeight // (self.tile_size + self.margin)) - 20 
-        self.grid = [[Tile((x * (self.tile_size + self.margin)) + self.margin, ((y * (self.tile_size + self.margin)) + self.margin) + 235 , self.tile_size, self.tile_size, (255,255,255), False,False,False) for y in range(self.grid_height)] for x in range(self.grid_width)]
+        self.grid = [[Tile((x * (self.tile_size + self.margin)) + self.margin, ((y * (self.tile_size + self.margin)) + self.margin) + 235 , self.tile_size, self.tile_size, (255,255,255), False,False,False,False) for y in range(self.grid_height)] for x in range(self.grid_width)]
         
     def draw_board(self, screen):
         for row in self.grid:
@@ -29,6 +29,8 @@ class GameBoard:
                     matrix[i][j] = 'P'
                 elif self.grid[i][j].goalFlags:
                     matrix[i][j] = 'G'
+                elif self.grid[i][j].visitedFlags:
+                    matrix[i][j] = 'V'
         return matrix
     
     def get_Player_Pos(self):
@@ -38,6 +40,14 @@ class GameBoard:
                     return i,j
         print("No existent Player in the board")
 
-
+    def get_Goal_Pos(self):
+        for i in range(self.grid_width):
+            for j in range(self.grid_height):
+                if self.grid[i][j].goalFlags:
+                    return i,j
+        print("No existent Goal in the board")
+    
+    def getNeighbours(self,x,y):
+        return [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
 
