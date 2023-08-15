b637d272542a75bdd9d3c1190928b627ee42b1fd
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-08-14 22:18:40 -0600

    Add files via upload

--- tile.py
+++ tile.py
@@ -1,12 +1,13 @@
 import pygame
 
 class Tile(pygame.Rect):
-    def __init__(self, x, y, width, height, color, blockFlags, playerFlags, goalFlags):
+    def __init__(self, x, y, width, height, color, blockFlags, playerFlags, goalFlags, visitedFlags):
         super().__init__(x, y, width, height)
         self.color = color
         self.blockFlags = blockFlags
         self.playerFlags = playerFlags
         self.goalFlags = goalFlags
+        self.visitedFlags = visitedFlags
         
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self)
